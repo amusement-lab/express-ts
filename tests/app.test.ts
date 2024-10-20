809373021d3004d1c1fe7b2470e70a173ad0b3ea
dipadana <dipadanadev@gmail.com>
2024-10-20 13:55:32 +0800

    test: add vitest as test runner

---
+++ tests/app.test.ts
@@ -0,0 +1,20 @@
+import { describe, it, expect } from 'vitest'
+import superagent from 'superagent'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+const url = process.env.API_URL!
+
+describe('Testing connection to API server', () => {
+  it('GET /', async () => {
+    const res = await superagent.get(url)
+
+    expect(res.statusCode).to.equal(200)
+
+    expect(res.body).to.be.an('object')
+    expect(res.body).to.have.property('message')
+
+    expect(res.body.message).to.be.an('string')
+    expect(res.body.message).to.equal('Express + TypeScript')
+  })
+})
