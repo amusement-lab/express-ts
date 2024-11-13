357c4838f7aa81903321aabf6e25fbff1af09ae5
dipadana <dipadanadev@gmail.com>
2024-11-13 13:03:25 +0800

    refactor: add typing for router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,7 @@
 import { Router, Response, Request } from 'express'
 import TestController from '../controllers/testController'
 
-const router = Router()
+const router: Router = Router()
 
 router.get('/', (_: Request, res: Response) => {
   res.status(200).json({ message: 'Express + TypeScript' })
